f0b70f3e2189eb7e7f038610b578cf46ba487aa8
Christian Fuchs <chris@c-macmini.local>
2024-12-26 18:46:37 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# feed_my_wled
-A Python script that transforms an audio stream to feed WLED over the air using its audio-reactive feature with Mac/Linux.
+# Feed My WLED
+A Python script that transforms an audio stream to feed WLED over the air using its audio-reactive feature.
+
+## Why?
+Version 0.15 ("Kösen") of WLED (link) comes with a built-in audio-reactive feature, which was previously a fork. But how can you use it? At first glance, all I found was usage via analog/digital microphones or wired add-ons. Really? That was never an option for me because I don’t want background noise displayed on my LED strip, and I’m certainly not going to lay another cable around the room. After digging into the source code, it turns out WLED accepts specialized UDP packets (WARSL2 Protocol). On Windows, you may use this: [WledSRServer](https://github.com/Victoare/SR-WLED-audio-server-win). But what if you use Mac or Linux? That’s why I created this script, which can be fed with an audio stream and outputs the right data as a network UDP stream to your WLED strip.
+
+## Working Principle on macOS
+* Play some music (e.g., Apple Music) and share audio with your regular output AND Shairport-Sync.
+* Configure Shairport-Sync to output audio as a FIFO stream — this can be done on the same Mac.
+* Feed your stream to `feed_my_wled.py`.
+* WLED receives your stream and reacts to audio by choosing sound-reactive effects.
+
+## Working Principle on Linux
+To be honest, I know it will work but haven’t tested it yet. Here are some hints that might help:
+* Install Shairport-Sync (or a loopback device) and play audio through it.
+* Create a FIFO stream and pipe it to `feed_my_wled`.
+
+## How It Works
+`feed_my_wled.py` processes any (raw) audio stream:
+* Reads the audio stream from stdin.
+* Buffers the audio stream. The buffer length can be set—this feature helps with syncing. On a Mac, the stream is often ahead of the music on your speakers. Buffering resolves this issue, even during play/pause or skipping tracks.
+* Calculates raw level (4B float).
+* Calculates peak level (1B int).
+* Calculates FFT for 16 bands, normalizes, and converts it to 1B int.
+* Determines the dominant frequency.
+* Calculates the magnitude sum.
+* Creates a UDP packet with calculated data and fixed values (WARSL2 Protocol).
+* Sends it to any WLED device on the same network (ESP32 recommended as a receiver).
+
+## Preferences
+* `WLED_IP_ADDRESS`: IP address of your WLED device.
+* `WLED_UDP_PORT`: Port (default is 11988).
+* `sample_rate`: Sample rate of your audio stream. Shairport-Sync’s default is 88200 (it took me hours to figure this out!).
+* `buffer_size`: Default is 163840, creating a delay of ~1.5 seconds. Adjust this variable for perfect synchronization. Higher values result in more delay.
+* `chunk_size`: Bytes read by the script at once. Higher values improve mean calculations and reduce network bandwidth usage. The default is 8192, resulting in ~25 packets per second, which is sufficient for a fluid experience.
+
+## Setup (on a Mac)
+I recommend using Homebrew for all installations.
+
+1. Install Python 3 and its dependencies (if not already installed):
+   * numpy
+   * pyaudio
+2. Download and install Shairport-Sync (link) on your Mac.
+3. Edit the Shairport-Sync config file (`/usr/local/etc/shairport-sync/shairport-sync.conf` on Mac) as follows:
+
+```json
+{
+  "general": {
+    "name": "Shairport",
+    "output_backend": "pipe",
+    "port": 6000,
+    "ignore_volume_control": "no",
+    "audio_backend_latency_offset_in_seconds": 0.0,
+    "run_this_before_play_begins": "<path_to_your/feed_my_wled.py>"
+  },
+  "pipe": {
+    "name": "/tmp/shairport-sync-audio"
+  }
+}
+```
+
+4. Start Shairport (`-d` as daemon, `-k` to kill it): `shairport-sync -d`
+5. Clone or download `feed_my_wled.py` and save it somewhere on your Mac.
+6. Pipe the stream to `feed_my_wled.py`: `cat /tmp/shairport-sync-audio | ./feed_my_wled.py`
+7. Configure your WLED device:
+   * Settings → WiFi: Disable WiFi sleep: OFF.
+   * Settings → Sync → Realtime: Enable Receive UDP Realtime.
+   * Settings → Usermod → AudioReactive: Enabled ON.
+   * Settings → Usermod → AudioReactive → Sync: Port: 11988.
+   * Settings → Usermod → AudioReactive → Sync: Mode: Receive.
+   * Reboot WLED Device.
+   * Main page: Select any audio-reactive effect, tweak the settings, and enjoy the show!
+
+### About Me
+This is my first project on GitHub and also my first "real" project written in Python, a language I’ve never used before. So, if you see something weird or unusual, please have mercy and let me know how I can improve. Regards, Chris
---
+++ feed_my_wled.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import sys
+import configparser
+import socket
+import struct
+import numpy as np
+from collections import deque
+
+# load preferences file
+config = configparser.ConfigParser()
+config.read("feed_my_wled.conf")
+
+#load preferences
+WLED_IP_ADDRESS = config.get("WLED", "WLED_IP_ADDRESS")
+WLED_UDP_PORT = config.getint("WLED", "WLED_UDP_PORT")
+sample_rate = config.getint("Audio", "sample_rate")
+buffer_size = config.getint("Audio", "buffer_size")
+chunk_size = config.getint("Audio", "chunk_size")
+
+# def vars
+previous_smoothed_level = 0.0
+ring_buffer = deque(maxlen=buffer_size // chunk_size)  # Ringpuffer für Blöcke (standardmäßig leer)
+
+# create socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+## functions
+# calculating fft
+def calculate_fft(audio_chunk, sample_rate):
+    """
+    Calc FFT for a Audioblock
+    :param audio_chunk: Audiodata as Byte-Array.
+    :param sample_rate: Samplerate of Audiodata.
+    :return: Tuple (FFT-Ergebnisse for 16 Frequency bands, additional peaks).
+    """
+    try:
+        # Convert to a numpy-Array
+        audio_data = np.frombuffer(audio_chunk, dtype=np.int16)
+
+        # Calc Peak (Raw Level and Peak Level)
+        raw_level = np.mean(np.abs(audio_data))
+        peak_level = int((np.max(np.abs(audio_data)) / 32767) * 255)
+
+        # Calc FFT and Amplitudes
+        fft_result = np.abs(np.fft.rfft(audio_data))
+
+        # Normalize from 0 to 255
+        fft_normalized = np.interp(fft_result, (0, np.max(fft_result)), (0, 255))
+
+        # Select first 16 frequency bands
+        fft_values = fft_normalized[:16].astype(np.uint8)
+
+        # Find dominating frequency
+        freq_index = np.argmax(fft_result)
+        fft_peak_frequency = freq_index * (sample_rate / len(audio_data))
+
+        # sum of fft magnitudes
+        fft_magnitude_sum = np.sum(fft_result)
+
+        # return calced values
+        return fft_values, raw_level, peak_level, fft_magnitude_sum, fft_peak_frequency
+
+        #error handling
+    except Exception as e:
+        print(f"Error at calcing FFT: {e}")
+        return None, 0, 0, 0, 0, 0
+
+# function for creating the udp package
+def create_udp_packet(fft_values, raw_level, smoothed_level, peak_level, fft_magnitude_sum, fft_peak_frequency):
+    """
+    Creating udp-package in a wled compatible format
+    :param fft_values: fft datas for 16 frequency bands
+    :param raw_level: mean of audiosource
+    :param smoothed_level: smoothed level of audiosource
+    :param peak_level: mac peak of audiosource
+    :param fft_magnitude_sum: sum of fft magnitudes
+    :param fft_peak_frequency: dominant frequency of audiosignal
+    :return: formated UDP-package
+    """
+
+    # Convert Values
+    peak_level = int(peak_level)  # limit to uint8
+    fft_values = [int(v) for v in fft_values]  # limit to uint8
+
+    # Create package according to the WLED Protocol
+    udp_packet = struct.pack('<6s2B2fBB16B2B2f',        # don't mess with that!
+        b'00002',                   # Header (6 Bytes)
+        0,0,                        # Gap (2 Bytes)
+        float(raw_level),           # Raw Level (4 Bytes Float)
+        float(smoothed_level),      # Smoothed Level (4 Bytes Float)
+        peak_level,                 # Peak Level (1 Byte)
+        0,                          # static 0 (1 Byte)
+        *fft_values,                # FFT Result (16 Bytes)
+        0,0,                        # Gap (2 Bytes)
+        float(fft_magnitude_sum),   # FFT Magnitude (4 Bytes Float)
+        float(fft_peak_frequency))  # FFT Major Peak (4 Bytes Float)
+    return udp_packet
+
+# main function
+def stream_audio_to_wled():
+    """
+    Reads audiodata from stream and send analyzed fft results to WLED
+    """
+    global previous_smoothed_level, ring_buffer
+
+    # init buffer with zeros
+    for _ in range(ring_buffer.maxlen):
+        ring_buffer.append(b"\x00" * chunk_size)
+
+    try:
+        print(f"Start Pipe to WLED at ({WLED_IP_ADDRESS}:{WLED_UDP_PORT})...")
+
+        # open stream from pipe
+        while True:
+            # read datas from pipe
+            audio_data = sys.stdin.buffer.read(chunk_size)
+            if audio_data:
+                ring_buffer.append(audio_data)  # push data to buffer
+
+            # combine blocks for prozessing
+            combined_data = b"".join(ring_buffer)
+
+            # Calc FFT and Peaks with buffersize
+            fft_result = calculate_fft(combined_data[:chunk_size])
+            if fft_result[0] is None:
+                print("Unvalid FFT-Datas, skip actual block.")
+                continue
+
+            # feed fft_result to its vars
+            fft_data, raw_level, peak_level, fft_magnitude_sum, fft_peak_frequency = fft_result
+
+            # Smooth the Peaks (Moving Average)
+            smoothed_level = (0.8 * previous_smoothed_level) + (0.2 * raw_level)
+            previous_smoothed_level = smoothed_level
+
+            # Create UDP-Paket
+            udp_packet = create_udp_packet(fft_data, raw_level, smoothed_level, peak_level, fft_magnitude_sum, fft_peak_frequency)
+
+            # Send Package to WLED
+            udp_socket.sendto(udp_packet, (WLED_IP_ADDRESS, WLED_UDP_PORT))
+
+    except KeyboardInterrupt:
+        print("Audiostreaming closed.")
+    finally:
+        udp_socket.close()
+
+# start 
+stream_audio_to_wled()
